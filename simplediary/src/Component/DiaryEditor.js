92e9e58b2e5aae4bbc698aaa1095c4c0f9dee4c9
yikanghee <yikanghee2@naver.com>
2022-07-11 22:51:54 +0900

    공부

--- simplediary/src/Component/DiaryEditor.js
+++ simplediary/src/Component/DiaryEditor.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,14 +17,23 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log("저장성공")
-    alert("저장성공")
-  }
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      authorInput.current.focus();
+      return;
+    }
+    console.log("저장성공");
+    alert("저장성공");
+  };
   return (
     <div className="DiaryEditor">
       <h1>오늘의 일기</h1>
       <div>
         <input
+          ref={authorInput}
           placeholder="제목을 입력하세요"
           value={state.author}
           name="author"
@@ -31,6 +42,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={authorInput}
           placeholder="내용을 입력하세요"
           value={state.content}
           name="content"
@@ -38,6 +50,7 @@ const DiaryEditor = () => {
         />
       </div>
       <div>
+        오늘의 감정점수 :
         <select
           name="emotion"
           value={state.emotion}
